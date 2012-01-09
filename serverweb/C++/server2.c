8061266faa56e3319dc76ca54cc760b004e6790a
Kashack <kashack@kashack-C90S.(none)>
2012-01-09 11:32:46 +0100

    mise en place d'un dialogue serveur php / C++

--- serverweb/C++/server2.c
+++ serverweb/C++/server2.c
@@ -69,11 +69,22 @@ void dostuff (int sock)
 {
    int n;
    char buffer[256];
-      
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   if (n < 0) error("ERROR writing to socket");
+   for (;;)
+   {
+	   bzero(buffer,256);
+	   n = read(sock,buffer,255);
+	   if (n < 0)
+	   {
+	    error("ERROR reading from socket");
+	   }
+	   if (n > 0)
+	   {
+	    printf("Here is the message: %s\n",buffer);
+	   }
+	   n = write(sock,"I got your message",18);
+	   if (n < 0)
+	   {
+	    error("ERROR writing to socket");
+	   }
+   }
 }
