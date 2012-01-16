d47961a1a3d2ce58d8b8464de029d25504e54df4
Rémi Leveugle <remi@eduroinsa71.insa-lyon.fr>
2012-01-16 11:34:22 +0100

    test

--- serverweb/C++/server2.c
+++ serverweb/C++/server2.c
@@ -1,26 +1,32 @@
 /* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
+   The port number is passed as an argument
+   This version runs forever, forking off a separate
    process for each connection
 */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
-void dostuff(int); /* function prototype */
+void *dostuff(void*ptr); /* function prototype */
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
+
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, pid;
+	char *message1 = "Thread 1";
+     int sockfd,portno, pid;
+     int *newsockfd;
+     pthread_t thread1;
      socklen_t clilen;
      struct sockaddr_in serv_addr, cli_addr;
 
@@ -29,7 +35,7 @@ int main(int argc, char *argv[])
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
@@ -37,22 +43,18 @@ int main(int argc, char *argv[])
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0)
               error("ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
      while (1) {
-         newsockfd = accept(sockfd, 
+         newsockfd = accept(sockfd,
                (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
+         if (newsockfd < 0)
              error("ERROR on accept");
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
-         if (pid == 0)  {
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
+         int reussite;
+         reussite = pthread_create(&thread1, NULL, dostuff, (void*) newsockfd);
+
          }
          else close(newsockfd);
      } /* end of while */
@@ -61,12 +63,14 @@ int main(int argc, char *argv[])
 }
 
 /******** DOSTUFF() *********************
- There is a separate instance of this function 
+ There is a separate instance of this function
  for each connection.  It handles all communication
  once a connnection has been established.
  *****************************************/
-void dostuff (int sock)
+void *dostuff (void*ptr)
 {
+   int sock;
+   sock = (int)*ptr;
    int n;
    char buffer[256];
    for (;;)
