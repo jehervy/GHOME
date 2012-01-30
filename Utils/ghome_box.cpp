9dfe69241b03a8d3933af6a0b9ea0f5520840a18
Miroof <miroof@miroof-G53JW.(none)>
2012-01-30 08:45:57 +0100

    new ghome_box

--- Utils/ghome_box.cpp
+++ Utils/ghome_box.cpp
@@ -7,7 +7,6 @@
 
 #include "ghome_box.h"
 #include <iostream>
-using namespace std;
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,7 +14,7 @@ using namespace std;
 #include<sys/errno.h>
 #include <sys/msg.h>
 
-void ghome_box::receive_message(int box, int &typeMes, int &metric, int &room, int &value)
+bool ghome_box::receive_message(int box, int &typeMes, int &metric, int &room, int &value)
 {
 	int rc;
 	int intMsg[4];
@@ -26,6 +25,7 @@ void ghome_box::receive_message(int box, int &typeMes, int &metric, int &room, i
 	{
 		perror( strerror(errno) );
 		printf("msgrcv failed, rc=%d\n", rc);
+		return false;
 
 	}
 	else
@@ -41,10 +41,12 @@ void ghome_box::receive_message(int box, int &typeMes, int &metric, int &room, i
 		metric = intMsg[1];
 		room = intMsg[2];
 		value = intMsg[3];
+
+		return true;
 	}
 }
 
-void ghome_box::send_actuator_box(int box,const int typeMes, const int metric, const int room, const int value ){
+bool ghome_box::send_actuator_box(int box,const int typeMes, const int metric, const int room, const int value ){
 	struct msg_buf msg;
 	bzero(msg.mtext, MSGTXTLEN);
 	int rc;
@@ -67,6 +69,66 @@ void ghome_box::send_actuator_box(int box,const int typeMes, const int metric, c
 	{
 		perror( strerror(errno) );
 		printf("msgsnd failed, rc = %d", rc);
+		return false;
 
+	}
+
+	return true;
+}
+
+bool ghome_box::send_message(int box, const int int1, const int int2)
+{
+	struct msg_buf msg;
+	bzero(msg.mtext, MSGTXTLEN);
+	int rc;
+	int intMsg[2];
+	msg.mtype = 1; // set the type of message
+
+	intMsg[0] = int1;
+	intMsg[1] = int2;
+
+	memcpy (&msg.mtext, &intMsg[0], sizeof(intMsg[0]));
+	memcpy (&msg.mtext[4], &intMsg[1], sizeof(intMsg[1]));
+
+	// send the message to queue
+	rc = msgsnd(box, &msg, sizeof(msg.mtext), 0 | MSG_NOERROR); // the last param can be: 0, IPC_NOWAIT, MSG_NOERROR, or IPC_NOWAIT|MSG_NOERROR.
+	if (rc < 0)
+	{
+		perror( strerror(errno) );
+		printf("msgsnd failed, rc = %d", rc);
+
+		return false;
+
+	}
+
+	return true;
+}
+
+bool ghome_box::receive_message(int box, int &int1, int &int2)
+{
+	int rc;
+	int intMsg[2];
+	struct msg_buf msg;
+	bzero(msg.mtext, MSGTXTLEN);
+	rc = msgrcv(box, &msg, sizeof(msg.mtext), 0, 0);
+	if (rc < 0)
+	{
+		perror( strerror(errno) );
+		printf("msgrcv failed, rc=%d\n", rc);
+
+		return false;
+
+	}
+	else
+	{
+		bzero(intMsg, sizeof(intMsg));
+
+		memcpy (&intMsg[0], &msg.mtext, sizeof(intMsg[0]));
+		memcpy ( &intMsg[1],&msg.mtext[4], sizeof(intMsg[1]));
+
+		int1 = intMsg[0];
+		int2 = intMsg[1];
+
+		return true;
 	}
 }
