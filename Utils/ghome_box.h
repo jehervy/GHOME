dc7d0589000f5ab0f550356a9d8170e281aae684
rleveugle <remi.leveugle@insa-lyon.fr>
2012-02-02 11:15:51 +0100

    Update Utils/ghome_box.h

--- Utils/ghome_box.h
+++ Utils/ghome_box.h
@@ -19,9 +19,23 @@ struct msg_buf {
 class ghome_box {
 public:
 	ghome_box(){};
+		/*
+	 * Constructeur
+	 */
 	virtual ~ghome_box(){};
+		/*
+	 * Destructeur
+	 */
     static bool send_actuator_box(int box,const int typeMes, const int metric, const int room, const int value);
+        /*
+     * Place un nouveau message dans une file de message
+     * passee en parametre
+     */
     static bool receive_message(int box, int &typeMes, int &metric, int &room, int &value);
+      /*
+     * Lis le premier message dans une file de message
+     * passee en parametre
+     */
 
     static bool send_message(int box, const int int1, const int int2);
     static bool receive_message(int box, int &int1, int &int2);
