a68f9bc01333103fd67324fa361552e23f3bc3ae
rleveugle <remi.leveugle@insa-lyon.fr>
2012-02-02 11:13:25 +0100

    Update ServerPere/communication_client.h

--- ServerPere/communication_client.h
+++ ServerPere/communication_client.h
@@ -9,20 +9,37 @@
 #define COMMUNICATIONCLIENT_H_
 #include <pthread.h>
 #include <iostream>
+using namespace std;
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "server_pere.h"
-using namespace std;
+#include "../Utils/ghome_box.h"
 
 
 class communication_client {
 public:
 	communication_client();
-	communication_client(int sensorServerBox, int actuatorServerBox, int fd, int sock);
+	/*
+	 * Constructeur
+	 */
+	communication_client(int sensorServerBox, int actuatorServerBox, int fd, int sock, void * ptr);
+	/*
+	 * Constructeur
+	 */
 	virtual ~communication_client();
+	/*
+	 * Destructeur
+	 */
 	void transfer_message();
+	/*
+	 * Lis les messages envoyés sur le socket
+	 */
 	void free_create_buffer(int longueur);
+	/*
+	 * Gere la creation et la liberation d'un buffer.
+	 * Prend en parametre la taille du buffer cree.
+	 */
 private :
 	int p_sensorServerBox;
 	int p_actuatorServerBox;
@@ -30,9 +47,11 @@ private :
 	bool p_opened;
 	int p_sock;
 	char * buffer;
+	int id;
 	int metric;
 	int room;
 	int value;
+
 };
 
 #endif /* COMMUNICATIONCLIENT_H_ */
