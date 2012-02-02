a127583a9b6b2f0d195a9543cf1cda317dd00cfa
rleveugle <remi.leveugle@insa-lyon.fr>
2012-02-02 11:13:41 +0100

    Update ServerPere/communication_client.cpp

--- ServerPere/communication_client.cpp
+++ ServerPere/communication_client.cpp
@@ -11,23 +11,44 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-communication_client::communication_client() {
+server_pere* papa;
+
+communication_client::communication_client()
+/*
+ * Constructeur
+ */
+{
 
 
 }
 
-communication_client::communication_client(int sensorServerBox, int actuatorServerBox, int fd, int sock) :
-p_sensorServerBox(sensorServerBox), p_actuatorServerBox(actuatorServerBox), p_fd(fd), p_sock(sock){
-transfer_message();
+communication_client::communication_client(int sensorServerBox, int actuatorServerBox, int fd, int sock, void * ptr) :
+p_sensorServerBox(sensorServerBox), p_actuatorServerBox(actuatorServerBox), p_fd(fd), p_sock(sock)
+/*
+ * Constructeur surcharge.
+ * Appel la methode de gestion des messages lues sur le socket
+ */
+{
+	papa = (server_pere*)ptr;
+	transfer_message();
 
 }
 
-communication_client::~communication_client() {
+communication_client::~communication_client()
+/*
+ * Destructeur
+ */
+{
 
 }
 
 void communication_client::free_create_buffer(int longueur)
+/*
+ * Prends en parametre la longueur du buffer à initialisé,
+ * après sa liberation memoire.
+ */
 {
 	free(buffer);
 	buffer = (char*) malloc(longueur);
@@ -35,80 +56,83 @@ void communication_client::free_create_buffer(int longueur)
 }
 
 void communication_client::transfer_message()
-{
-	cout << "On est dans la communication client" << endl;
-	cout <<p_fd<<endl;
-	   int n;
-
-	   p_opened=true;
-	   //int taille_message;
-
-	   /*Fonctionnement d'une lecture d'un ordre de pilotage
+/*
+ * Gere les messages recues sur le socket (envoyes par les clients).
+ * Fonctionnement d'une lecture d'un ordre de pilotage
 	    * d'un client :
+	    * - lecture sur un octet : type du message (info, pilotage,...)
+	    * 		- id = 0 : deconnexion du client
+	    * 		- id = 4 : ordre de fermeture du server
+	    * 		- id = 1 : info
+	    * 		- id = 2 : pilotage
+	    * 		- id = 3 : ordre de maintenance
 	    * - lecture sur un octet : taille du message de metric
 	    * - lecture du metric
 	    * - lecture sur un octer : taille du message de room
 	    * - lecture du room
 	    * - lecture sur un octet : taille du message de value
-	    */
+ */
+{
+
+	   int n;
+	   p_opened=true;
 	   int t_longueur = 1;
 	   int m_longueur;
 while(p_opened)
 {
 	communication_client::free_create_buffer(t_longueur);
-	n = read(p_fd, buffer, t_longueur);
-	if(n>0)
-	{
+	n=read(p_fd, buffer, t_longueur);
+	id=atoi(buffer);
+	communication_client::free_create_buffer(t_longueur);
+	cout<<"ID : "<<id<<endl;
+	switch(id){
+	case 4 :
+		p_opened=false;
+		papa->set_opened(false);
+		papa->kill_thread();
+		break;
+	case 0 :
+		p_opened=false;
+		papa->delete_fd(p_fd);
+		break;
+	case 1 :
+		break;
+	case 2 :
+		n = read(p_fd, buffer, t_longueur);
 		m_longueur = atoi(buffer);
 		communication_client::free_create_buffer(m_longueur);
+		//Lecture de metric
 		n = read(p_fd, buffer, m_longueur);
 		metric = atoi(buffer);
-		if(metric!=0)
-		{
-			communication_client::free_create_buffer(t_longueur);
-			n = read(p_fd, buffer, t_longueur);
-			m_longueur = atoi(buffer);
-			communication_client::free_create_buffer(m_longueur);
-			n = read(p_fd, buffer, m_longueur);
-			room = atoi(buffer);
-			communication_client::free_create_buffer(t_longueur);
-			n = read(p_fd, buffer, t_longueur);
-			m_longueur = atoi(buffer);
-			communication_client::free_create_buffer(m_longueur);
-			n = read(p_fd, buffer, m_longueur);
-			value = atoi(buffer);
+		communication_client::free_create_buffer(t_longueur);
+		//Nombre d'octets pour room
+		n = read(p_fd, buffer, t_longueur);
+		m_longueur = atoi(buffer);
+		communication_client::free_create_buffer(m_longueur);
+		//Lecture de room
+		n = read(p_fd, buffer, m_longueur);
+		room = atoi(buffer);
+		communication_client::free_create_buffer(t_longueur);
+		//Nombre d'octets pour value
+		n = read(p_fd, buffer, t_longueur);
+		m_longueur = atoi(buffer);
+		communication_client::free_create_buffer(m_longueur);
+		//Lecture de value
+		n = read(p_fd, buffer, m_longueur);
+		value = atoi(buffer);
 
-			cout << "Metric : "<<metric<<endl;
-			cout << "Room : "<<room<<endl;
-			cout << "Value : "<<value<<endl;
-		} else {
-			//On souhaite fermer le socket
-			p_opened=false;
-			server_pere::s_opened=false;
-			cout << "Ferme : "<<server_pere::s_opened <<endl;
-		}
+		//Ecriture du message dans la boite aux lettres actuator
+		ghome_box::send_actuator_box(p_actuatorServerBox, id, metric, room, value);
+
+		break;
+	case 3 :
+		break;
 
 	}
-		  /* if (n > 0)
-		   {
-			cout << "Nb octets : " << n << endl;
-			string type_2(buffer);
-			if(type_2=="0")
-			{
-				p_opened=false;
-				server_pere::s_opened=false;
-				cout << "Ferme : "<<server_pere::s_opened <<endl;
-			}
-				printf("Here is the message: %d\n", atoi(buffer));
 
-
-		   }*/
-		   n = write(p_fd,"I got your message",18);
 
 
 
 }
-cout<<"Sortie du premier while"<<endl;
-close(p_fd);
 
 }
