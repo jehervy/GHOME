2440821e7062911cd6c86a986c3d68297f8d9946
Vincent <vincent.primault@gmail.com>
2012-02-16 12:33:00 +0100

    Active GMEM !

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 //Define GMEM to use the custom memory allocator module. If not defined, the
 //standard functions will be used instead.
-//#define GMEM
+#define GMEM
 
 //Define TESTING to switch to test mode.
 //#define TESTING
